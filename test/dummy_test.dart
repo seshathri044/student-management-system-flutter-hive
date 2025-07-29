28c4a995f9e2454a705b6a02e71cda5e9675a0b0
seshathri <m.seshathri.code@gmail.com>
2025-07-29 16:50:30 +0530

    Added test folder for CI

--- test/dummy_test.dart
+++ test/dummy_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Dummy test should always pass', () {
-    expect(2 + 2, 4);
+  test('Dummy test always passes', () {
+    expect(1 + 1, 2);
   });
 }
